01892eb363762d1a2bed4ab410a5b6344d996119
itsrv23 <nifirus@gmail.com>
2022-10-25 21:17:57 +0300

    init

---
+++ src/main/java/com/tgshelterbot/TgShelterBotApplication.java
@@ -0,0 +1,13 @@
+package com.tgshelterbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgShelterBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TgShelterBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tgshelterbot/TgShelterBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tgshelterbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgShelterBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
