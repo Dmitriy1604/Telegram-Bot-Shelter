60570c420b7a0237e1e595982dbd144b8bf8e7f3
Filimonov Sergey <serifil1282@gmail.com>
2022-11-13 17:52:04 +0500

    Report reminder bug fix, Exception handler extend functionality, New AnimalDecisionController Animal class new Enum was added

--- src/main/java/com/tgshelterbot/repository/AnimalRepository.java
+++ src/main/java/com/tgshelterbot/repository/AnimalRepository.java
@@ -10,8 +10,10 @@ import java.util.List;
 
 public interface AnimalRepository extends JpaRepository<Animal, Long> {
     List<Animal> findAnimalsByUserId(Long id);
-    @Query(value = "select a.id as id, a.name as animal, s.name as shelter  from animals a inner join shelters s on a.shelter_id = s.id order by a.id",nativeQuery = true)
+
+    @Query(value = "select a.id as id, a.name as animal, s.name as shelter  from animals a inner join shelters s on a.shelter_id = s.id order by a.id",
+            nativeQuery = true)
     List<AnimalsSimple> findAllAnimalsSimple();
 
-    Collection<Animal> findAllByUserIdNotNull();
+    Collection<Animal> findAllByUserIdNotNullAndState(Animal.AnimalStateEnum stateEnum);
 }
